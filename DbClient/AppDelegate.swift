1119fef7e111d9e9a9f9e176421641e45e245362
fajdof <fajdof@gmail.com>
2016-11-05 16:36:04 +0100

    Initial Commit

---
+++ DbClient/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DbClient
+//
+//  Created by Filip Fajdetic on 05/11/2016.
+//  Copyright © 2016 Filip Fajdetic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
