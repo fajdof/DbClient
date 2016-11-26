868ac6b6d37fa927620e07a757043955353b2638
fajdof <fajdof@gmail.com>
2016-11-26 11:57:15 +0100

    Cosmetic changes

--- DbClient/GeneralExtensions.swift
+++ DbClient/GeneralExtensions.swift
@@ -6,31 +6,16 @@
 //  Copyright © 2016 Filip Fajdetic. All rights reserved.
 //
 
-
-//				client?.execute("SELECT DISTINCT Stavka.* FROM Artikl LEFT JOIN Stavka ON Stavka.SifArtikla = " + item.code!.description + " WHERE Stavka.SifArtikla <> NULL", completion: { (data) in
-//					if let dbData = data {
-//						for table in dbData {
-//							if let tab = table as? Array<Dictionary<String, AnyObject>> {
-//								for row in tab {
-//									if let unit = Unit(JSON: row) {
-//										item.units.append(unit)
-//									}
-//								}
-//							}
-//						}
-//					}
-//				})
-
 import Foundation
 import Cocoa
 
 extension NSTextField {
 	
 	func addAttributedString(_ nameString: String, dataString: String?) {
-		let codeAttString = NSMutableAttributedString(string: nameString + (dataString ?? ""))
-		codeAttString.addAttribute(NSFontAttributeName, value: NSFont.boldSystemFont(ofSize: 14), range: NSRange(location: 0, length: nameString.characters.count))
+		let mutableAttString = NSMutableAttributedString(string: nameString + (dataString ?? ""))
+		mutableAttString.addAttribute(NSFontAttributeName, value: NSFont.boldSystemFont(ofSize: 14), range: NSRange(location: 0, length: nameString.characters.count))
 		
-		attributedStringValue = codeAttString
+		attributedStringValue = mutableAttString
 	}
 	
 }
